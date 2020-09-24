994e3182c63ebddd417016f1c5cf4842fa8a0315
Mihaita Tinta <mihaita.tinta@ing.com>
2020-09-24 14:59:38 +0300

    test replaced mocks

--- src/test/java/com/mih/yolt/client/services/SiteServiceTest.java
+++ src/test/java/com/mih/yolt/client/services/SiteServiceTest.java
@@ -11,25 +11,20 @@ import reactor.core.publisher.Mono;
 
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = {HttpConfiguration.class, SiteService.class})
+@SpringBootTest(classes = {HttpConfiguration.class, SiteService.class, AccessTokenService.class, RequestTokenService.class})
 class SiteServiceTest {
 
     @Autowired
     SiteService service;
 
-    @MockBean
+    @Autowired
     AccessTokenService accessTokenService;
-    @MockBean
+    @Autowired
     RequestTokenService requestTokenService;
 
     @Test
     public void test() {
-        String token = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..a-3lT69lsWEDRxh7cBIYhw.FDPhvvOwTZbAkvN3fQx3_L-3ztbwwVzShvcqYRSPINe0MSXMBQszYrHK3p3GbKSimbrNkAg8skJ-IZJpfKZC54tS2RgayFE-zWOZmCxVCF2hHM_M66IOy4w9Xed7-0yaPdlbMquC_cA700I-_xkH-E0aI1Zb3gUqoe_cHUOwoKa3zN_6JPT-lIBNcqpQEO6BeNQaRBts2pEjdIcs_3M9Zw.WX3OAsoIfMjFGJQRkZ76EDTiXDgCXmlWR2jpNOKA8u4";
-
-        when(accessTokenService.getAccessToken(ArgumentMatchers.any()))
-                .thenReturn(Mono.just(AccessToken.of(token)));
-
-        service.getSites()
+       service.getSites()
                 .log()
                 .block();
     }
